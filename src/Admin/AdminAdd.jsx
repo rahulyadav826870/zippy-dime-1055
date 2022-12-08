a486869d76f6ecd0322eb898d90b949a474a520b
rahulyadav826870 <rahulyadav826870@gmail.com>
2022-12-09 00:58:39 +0530

    created own api

--- src/Admin/AdminAdd.jsx
+++ src/Admin/AdminAdd.jsx
@@ -1,95 +1,101 @@
-import React from 'react'
-import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
-import { useEffect,useState } from 'react';
-import { Input,Button } from '@chakra-ui/react'
-import AdminBtn from './AdminBtn';
+import React from "react";
+import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Input, Button } from "@chakra-ui/react";
+import AdminBtn from "./AdminBtn";
 // import { Button } from '@chakra-ui/react/dist';
 
-export default function AdminAdd({page}) {
- // REACT use
- const [text, setText] = useState("")
- const [price,setPrice] =useState("")
- const [todo, setTodo] = useState([])
- const [loading, setLoading] = useState(false)
- const [toggle,setToggle] =useState(false)
+export default function AdminAdd({ page }) {
+  // REACT use
+  const [text, setText] = useState("");
+  const [price, setPrice] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
+  const newTodo = (e) => {
+    setText(e.target.value);
+  };
 
- const newTodo = (e) => {
-   setText(e.target.value)
- }
+  const getData = async (page) => {
+    setLoading(true);
+    let res = await fetch(
+      `https://trawel-admin-api.onrender.com/posts?_limit=5&_page=${page}`
+    );
+    let data = await res.json();
+    // console.log("data: ", data);
+    setTodo(data);
+    setLoading(false);
+  };
 
- const getData = async (page) => {
-   setLoading(true)
-   let res = await fetch(`http://localhost:3009/posts?_limit=5&_page=${page}`)
-   let data = await res.json()
-   // console.log("data: ", data);
-   setTodo(data)
-   setLoading(false)
- }
+  useEffect(() => {
+    // sendData()
+    getData(page);
+  }, [page]);
 
- 
-useEffect(() => {
-  // sendData()
-  getData(page)
+  if (loading) {
+    return <CircularProgress max={100} isIndeterminate color="green.300" />;
+  }
 
-}, [page])
+  const sendData = async () => {
+    setLoading(true);
+    const newItem = {
+      id: Math.floor(Math.random() * 100) + text,
+      title: text,
+      status: false,
+      price: price,
+    };
+    let res = await fetch(`https://trawel-admin-api.onrender.com/posts`, {
+      method: "POST",
+      body: JSON.stringify(newItem),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-if (loading) {
-  return <CircularProgress max={100} isIndeterminate color='green.300' />
+    let data = await res.json();
+    console.log(data);
+    setTodo([...todo, data]);
+    setLoading(false);
+    setText("");
+    setPrice("");
+    // getData()
+  };
 
-        
-}
-
-
-
- const sendData = async () => {
-
-   setLoading(true)
-   const newItem = {
-     id: Math.floor(Math.random() * 100) + text,
-     title: text,
-     status:false,
-     price:price
-   }
-   let res = await fetch(`http://localhost:3009/posts`, {
-     method: "POST",
-     body: JSON.stringify(newItem),
-     headers: {
-       "Content-Type": "application/json"
-     }
-   })
-
-   let data = await res.json()
-   console.log(data)
-   setTodo([...todo, data])
-   setLoading(false)
-   setText("")
-   setPrice("")
-   // getData()
-
-
- }
-
-
-
- // console.log(todo)
+  // console.log(todo)
 
   return (
     <div>
-<div style={{    display: "flex",
-    justifyContent: "space-around",
-    margin: "33px"}}>
-<Input style={{margin: "0px 12% 0px 0px"}} placeholder="Location Name" value={text} onChange={newTodo} />
-<Input placeholder="Price"  type="number" value={price} onChange={(e)=>setPrice(e.target.value)} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "33px",
+        }}
+      >
+        <Input
+          style={{ margin: "0px 12% 0px 0px" }}
+          placeholder="Location Name"
+          value={text}
+          onChange={newTodo}
+        />
+        <Input
+          placeholder="Price"
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </div>
 
-</div>
+      <Button
+        style={{ display: "block", marginBottom: "27px", marginLeft: "2%" }}
+        colorScheme="gray"
+        onClick={sendData}
+      >
+        ADD
+      </Button>
 
-      <Button style={{display: "block",
-    marginBottom: "27px",
-    marginLeft: "2%"}} colorScheme='gray' onClick={sendData}>ADD</Button>
-    
-     < AdminBtn getData={getData} todo={todo} />
-
+      <AdminBtn getData={getData} todo={todo} />
     </div>
-  )
+  );
 }
--- src/Admin/AdminBtn.jsx
+++ src/Admin/AdminBtn.jsx
@@ -1,5 +1,5 @@
 // import { Td } from '@chakra-ui/react/dist';
-import React from 'react'
+import React from "react";
 import {
   Table,
   Thead,
@@ -11,95 +11,80 @@ import {
   TableCaption,
   TableContainer,
   Button,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
-export default function AdminBtn({getData,todo}) {
+export default function AdminBtn({ getData, todo }) {
+  /// TOGGLE STATUS
 
-
-    
- /// TOGGLE STATUS
-
- const handleToggle =async(id,status) => {
-
+  const handleToggle = async (id, status) => {
     // console.log(typeof(newStatus))
-    let res = await fetch(`http://localhost:3009/posts/${id}`,{
-      method : 'PATCH',
-      body : JSON.stringify({ status :!status}),
-      headers:{
-        "Content-Type":"application/json"
-      }
- 
-    })
- 
-    let data1 = await res.json()
+    let res = await fetch(`https://trawel-admin-api.onrender.com/posts/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ status: !status }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let data1 = await res.json();
     console.log("data1: ", data1);
- 
+
     // setTodo(data1)
-    getData()
-  }
- 
- 
- 
- 
- // TOGGLE DELETE
-  const handleDelete=async (id)=>{
- 
- let res = await fetch(`http://localhost:3009/posts/${id}`,{
-  method :"DELETE",
-  headers:{
-    "Content-Type" :"application/json"
-  }
-  
- })
- 
- let data = await res.json()
- getData()
-  }
+    getData();
+  };
+
+  // TOGGLE DELETE
+  const handleDelete = async (id) => {
+    let res = await fetch(`https://trawel-admin-api.onrender.com/posts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let data = await res.json();
+    getData();
+  };
 
   return (
     <div>
+      <TableContainer m={10}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Location Name</Th>
+              <Th>Price</Th>
 
+              <Th>Status</Th>
+              <Th>Action</Th>
+              {/* <Th isNumeric>multiply by</Th> */}
+            </Tr>
+          </Thead>
 
-<TableContainer m={10}>
-  <Table variant='simple'>
-   
-    <Thead>
-      <Tr>
-        <Th>Location Name</Th>
-        <Th>Price</Th>
-
-        <Th>Status</Th>
-        <Th>Action</Th>
-        {/* <Th isNumeric>multiply by</Th> */}
-      </Tr>
-    </Thead>
-
-
-
-{ todo.map(el=>
-    <Tbody>
-
-      
-      <Tr>
-        <Td>{el.title}</Td>
-        <Td>{el.price}</Td>
-        <Td>
-        <Button colorScheme='green' onClick={()=>handleToggle(el.id,el.status)}>{el.status ? "Booked" : "Open"}</Button>
-
-        </Td>
-        <Td>
-        <Button colorScheme='red' onClick={()=>handleDelete(el.id)}>Delete</Button>
-        </Td>
-        {/* <Td isNumeric>25.4</Td> */}
-      </Tr>
-     
-    </Tbody>
-    )
-}
- 
-  </Table>
-</TableContainer>
-
+          {todo.map((el) => (
+            <Tbody>
+              <Tr>
+                <Td>{el.title}</Td>
+                <Td>{el.price}</Td>
+                <Td>
+                  <Button
+                    colorScheme="green"
+                    onClick={() => handleToggle(el.id, el.status)}
+                  >
+                    {el.status ? "Booked" : "Open"}
+                  </Button>
+                </Td>
+                <Td>
+                  <Button colorScheme="red" onClick={() => handleDelete(el.id)}>
+                    Delete
+                  </Button>
+                </Td>
+                {/* <Td isNumeric>25.4</Td> */}
+              </Tr>
+            </Tbody>
+          ))}
+        </Table>
+      </TableContainer>
     </div>
-  )
+  );
 }
